8cd814e1eed69b65b313ce94ce570e8438b1c2b0
Marek Skopowski <marek.skopowski@flashlabs.pl>
2022-11-06 17:03:45 +0100

    Feature #6 | Mark read

--- internal/process/mainprocess/task/fetchmessages.go
+++ internal/process/mainprocess/task/fetchmessages.go
@@ -8,7 +8,7 @@ import (
 
 func FetchMessages(srv *gmail.Service) (*gmail.ListMessagesResponse, error) {
 	user := "me"
-	query := "is:unread"
+	query := "is:unread from:idealista.com"
 	r, err := srv.Users.Messages.List(user).Q(query).Do()
 	if err != nil {
 		return nil, errors.New(fmt.Sprintf("Unable to retrieve messages: %v", err))
