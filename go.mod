76138d4bef59c4ffd3a6840e3d8d44e230b617c9
Marek Skopowski <marek.skopowski@flashlabs.pl>
2023-11-18 20:03:55 +0100

    #94 | Implement Rootpath

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	cloud.google.com/go/compute v1.23.1 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/flashlabs/rootpath v1.0.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
--- internal/test/bootstrap.go
+++ internal/test/bootstrap.go
@@ -1,27 +1,5 @@
 package test
 
 import (
-	"flag"
-	"os"
-	"path/filepath"
-	"runtime"
-	"testing"
+	_ "github.com/flashlabs/rootpath"
 )
-
-const pathToRoot string = "../.."
-
-func init() {
-	testing.Init()
-	flag.Parse()
-
-	err := os.Chdir(rootPath())
-	if err != nil {
-		panic(err)
-	}
-}
-
-func rootPath() string {
-	_, currFile, _, _ := runtime.Caller(0)
-
-	return filepath.Join(filepath.Dir(currFile), pathToRoot)
-}
